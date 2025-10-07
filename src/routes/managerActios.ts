99116a83e6022f715112797317612bbf9b9c112f
IKUNDABAYO Placide <ikundabayoplacide500@gmail.com>
2025-10-07 13:08:14 +0200

    Employee can get all Mission by passing missionId

--- src/routes/managerActios.ts
+++ src/routes/managerActios.ts
@@ -8,7 +8,7 @@ import { validationMiddleware } from "../middlewares/validationMiddleware";
 const ActionsRouter=Router();
 
 ActionsRouter.post("/actions/create",authenticate,checkRoleMiddleware(["manager"]), validationMiddleware({type:"body",schema:createActionSchema}),createMissionAction);
-ActionsRouter.get("/actions",authenticate,checkRoleMiddleware(["manager"]), getAllActions);
+ActionsRouter.get("/actions",authenticate,checkRoleMiddleware(["manager","finance_manager"]), getAllActions);
 ActionsRouter.get("/actions/:missionId",authenticate,checkRoleMiddleware(["manager"]), getActionByMissionId);
 ActionsRouter.get("/actions/action/:actionId",authenticate,checkRoleMiddleware(["manager"]), getActionById);
 ActionsRouter.patch("/actions/action/:actionId",authenticate,checkRoleMiddleware(["finance_manager"]), FinanceUpdateMissionAction);
